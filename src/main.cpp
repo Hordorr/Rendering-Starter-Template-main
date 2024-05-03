71974eb04bf7417291ce834eb2c8fdb19b44edbc
glesage <1998glesage@gmail.com>
2024-05-03 10:36:50 +0200

    initialCommit

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include "opengl-framework/opengl-framework.hpp" // Inclue la librairie qui va nous servir à faire du rendu
+
+int main()
+{
+    // Initialisation
+    gl::init("TPs de Rendering"); // On crée une fenêtre et on choisit son nom
+    gl::maximize_window(); // On peut la maximiser si on veut
+    auto const triangle_mesh = gl::Mesh{{
+        .vertex_buffers = {{
+            .layout = {gl::VertexAttribute::Position2D{0}},
+            .data = {
+                -1.f,-1.f,
+                +1.f,-1.f,
+                0.f,+1.f
+            },
+        
+
+        }},
+    }};
+    while (gl::window_is_open())
+    {
+        glClearColor(0.f,0.f,1.f,1.f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        // Rendu à chaque frame
+    }
+}
+
