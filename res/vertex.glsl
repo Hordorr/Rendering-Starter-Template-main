c612abea51a33a05cfdd9a5543e9ed9cbb091b78
glesage <1998glesage@gmail.com>
2024-05-22 12:16:56 +0200

    Cube

--- res/vertex.glsl
+++ res/vertex.glsl
@@ -1,8 +1,8 @@
 #version 410
 
-layout(location = 0) in vec2 in_position;
+layout(location = 0) in vec3 in_position;
 uniform float currentTime;
 uniform mat4 view_projection_matrix;
 void main(){
-    gl_Position = view_projection_matrix * vec4(in_position,0.f,1.f);
+    gl_Position = view_projection_matrix * vec4(in_position,1.f);
 }
